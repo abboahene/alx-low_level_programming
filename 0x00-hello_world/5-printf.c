07f41ef4d3ee01bbcaa80eba7e03fe6cbf4f7fc3
Alfred Benefo Boahene <alfredboahene.7@gmail.com>
2022-12-09 09:09:38 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of this program
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
