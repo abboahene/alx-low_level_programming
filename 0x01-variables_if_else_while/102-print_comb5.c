069d192d4e896cdc864609c95961a541f315fdca
Alfred Benefo Boahene <alfredboahene.7@gmail.com>
2022-12-12 09:55:17 -0800

    102 combs

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,9 +12,9 @@ int main(void)
 	int i, j;
 
 
-	for (i = 0; i < 98; i++)
+	for (i = 0; i < 99; i++)
 	{
-		for (j = i + 1; j < 99; j++)
+		for (j = i + 1; j < 100; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
