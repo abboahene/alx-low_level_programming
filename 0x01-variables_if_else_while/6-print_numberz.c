9303eda40b2a1f437ad8b8a725a3bd02b3938b50
Alfred Benefo Boahene <alfredboahene.7@gmail.com>
2022-12-12 08:06:38 -0800

    put numbs

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Entry point of this program
+*
+* Return: alway 0
+*/
+int main(void)
+{
+	int i;
+
+
+	for (i = 0; i < 10; i++)
+	{
+
+		putchar(i + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
