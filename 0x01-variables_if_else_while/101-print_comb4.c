e5f6437450d82897801a8f82320dd470571ae712
Alfred Benefo Boahene <alfredboahene.7@gmail.com>
2022-12-12 09:22:32 -0800

    101 combs

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Entry point of this program
+*
+* Return: alway 0
+*/
+int main(void)
+{
+	int i, j, k;
+
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = i + 2; k < 10; k++)
+			{	
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i == 7 && j == 8 && k == 9)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
