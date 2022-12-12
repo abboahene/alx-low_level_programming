978ab11229ea12af01e93d34dc9582a3a4742379
Alfred Benefo Boahene <alfredboahene.7@gmail.com>
2022-12-12 07:35:21 -0800

    ABC

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Entry point of this program
+*
+* Return: alway 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
