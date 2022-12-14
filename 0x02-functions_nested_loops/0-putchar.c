c8cad9ef9ca3d67faa06b26bba30f4ecd19d11c9
Alfred Benefo Boahene <alfredboahene.7@gmail.com>
2022-12-14 15:37:47 -0800

    put

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Entry point of this program
+*
+* Return: alway 0
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
