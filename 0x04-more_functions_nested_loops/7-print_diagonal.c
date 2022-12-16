f299d5945e5db00344139dfddfe951778dd01b74
Alfred Benefo Boahene <alfredboahene.7@gmail.com>
2022-12-15 18:36:20 -0800

    diag

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_diagonal - print diagonal
+* @n: length of diagonal
+*/
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
