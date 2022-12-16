e74cece44f5da00ce9344651141e9893d87876b1
Alfred Benefo Boahene <alfredboahene.7@gmail.com>
2022-12-15 17:40:21 -0800

    print

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -2,11 +2,11 @@
 
 /*
 * _putchar - print char c to std output
-*@c: character
+* @c: character
 *
 * Return: -1 for err and 1 for success 
 */
 int _putchar(char c)
 {
-	write(1, &c, 1);
+	return write(1, &c, 1);
 }
