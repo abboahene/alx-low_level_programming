8f3141b80be4278397d156efe09ea68777ccc92c
Alfred Benefo Boahene <alfredboahene.7@gmail.com>
2022-12-16 00:18:20 -0800

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_square - print square
+* @n: size of square
+*/
+void print_square(int n)
+{
+	int i, j;
+
+	if (n < 1)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
