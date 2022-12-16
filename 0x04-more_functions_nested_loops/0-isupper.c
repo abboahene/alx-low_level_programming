42479ad4313c9435218986c3f9f4034d179ea34e
Alfred Benefo Boahene <alfredboahene.7@gmail.com>
2022-12-15 16:18:29 -0800

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+int _isupper(int c);
+
+/**
+* _isupper - Check if char is uppercase or not
+* @c: character
+*
+* Return: 1 - for uppercase and 0 not uppercase
+*/
+int _isupper(int c)
+{
+	if ( c < 97)
+	{
+		return (1)
+	}	
+	return (0);
+}
