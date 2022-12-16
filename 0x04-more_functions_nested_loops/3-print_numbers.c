bda002217948c633291994b759e41da8bba96c51
Alfred Benefo Boahene <alfredboahene.7@gmail.com>
2022-12-15 17:33:26 -0800

    print nnubs

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - print numbers
+*
+*/
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/*
+* _putchar - print char c to std output
+*@c: character
+*
+* Return: -1 for err and 1 for success 
+*/
+int _putchar(char c)
+{
+	write(1, &c, 1);
+}
